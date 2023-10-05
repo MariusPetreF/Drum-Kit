40a26afe2da520ee9c60d7006ca99bc5dbe06d0f
mariuspetref <mayutz96@gmail.com>
2023-10-06 02:08:49 +0300

    init

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
 
     switch (buttonInnerHTML) {
       case "w":
-        let audio = new Audio("sounds/tom-1.mp3");
+        let tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
         break;
       case "a":
-        let audio = new Audio("sounds/tom-2.mp3");
+        let tom2 = new Audio("sounds/tom-2.mp3");
         tom2.play();
         break;
       case "s":
-        let audio = new Audio("sounds/tom-3.mp3");
+        let tom3 = new Audio("sounds/tom-3.mp3");
         tom3.play();
         break;
       case "d":
-        let audio = new Audio("sounds/tom-4.mp3");
+        let tom4 = new Audio("sounds/tom-4.mp3");
         tom4.play();
         break;
       case "j":
-        let audio = new Audio("sounds/snare.mp3");
+        let snare = new Audio("sounds/snare.mp3");
         snare.play();
         break;
       case "k":
-        let audio = new Audio("sounds/crash.mp3");
+        let crash = new Audio("sounds/crash.mp3");
         crash.play();
         break;
       case "l":
-        let audio = new Audio("sounds/kick-bass.mp3");
-        kickBass.play();
+        let kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
         break;
 
       default:
